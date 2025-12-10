be5cb1807dd636765ebfc3808555fa0cd18f5a0f
Lyrids36 <2549306479@qq.com>
2025-12-10 17:20:48 +0800

    Refactor to use standard limits for data types
    Updated the program to use standard limits for int, long, and float types. Removed hardcoded max and min values and replaced them with appropriate macros.

--- Exercise/Chapter_3/1.c
+++ Exercise/Chapter_3/1.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
 int main(void)
 {
-    int int_max = 32767;
-    int int_max_1 = 32768;
-
-    long int long_max = 2147483647;
-    long int long_max_1 = 2147483648;
-
-    float float_max = 3.4e38f;
-    float float_max_1 = 3.4e38f * 100.0f;
-
-    float float_min = 0.1234E-38;
-    float float_min_1 = 0.1234E-38 / 10;
-
-    printf("int max = %d,int max + 1 = %d\n", int_max, int_max_1);
-    printf("long max = %ld,long max + 1 = %ld\n", long_max, long_max_1);
-    printf("float max = %e,float max + 1 = %e\n", float_max, float_max_1);
-    printf("float min = %e,float min - 1 =%e\n", float_min, float_min_1);
+    printf("int max = %d,int max + 1 = %d\n", INT_MAX, INT_MAX + 1);
+    printf("long max = %ld,long max + 1 = %ld\n", LONG_MAX, LONG_MAX + 1);
+    printf("float max = %e,float max * 10 = %e\n", FLT_MAX, FLT_MAX * 10);
+    printf("float min = %e,float min / 10 =%e\n", FLT_MIN, FLT_MIN / 10);
 
     return 0;
 }
